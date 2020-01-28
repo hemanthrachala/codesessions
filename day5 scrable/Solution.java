daf8e98428cf9b3b88fd876d7b37370a6624a3d1
hemanthrachala <hemanthrachala@gmail.com>
2020-01-28 23:58:19 +0530

    day5 done

---
+++ day5 scrable/Solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            String a = "";
+            String s = sc.nextLine();
+            String[] arr = s.split(" ");
+            for(int i = 0; i <arr.length ; i++) {
+                for(int j = arr[i].length() - 1 ; j >= 0 ; j-- ){
+                    a = a + arr[i].charAt(j);
+                    
+            }
+            a = a + " ";
+        }System.out.println(a);  
+        }
+    }
+}
